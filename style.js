35c5e239f7f00f5921cc009c665f5d9162418d56
Adarsh-Malav <47078002+Adarsh-Malav@users.noreply.github.com>
2023-09-14 21:51:16 +0530

    commit

---
+++ style.js
@@ -0,0 +1,8 @@
+function openNav(){
+    document.getElementById("accordian-down").style.height="270px"
+    document.getElementById("plus").style.display="none"
+
+}
+function closeNav(){
+    document.getElementById("cross").style.display="inline"
+}

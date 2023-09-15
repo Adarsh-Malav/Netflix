ec422a76809a3be9753a49549684f77306c8e0c9
Adarsh-Malav <47078002+Adarsh-Malav@users.noreply.github.com>
2023-09-15 10:02:54 +0530

    commit

--- style.js
+++ style.js
@@ -1,8 +1,22 @@
 function openNav(){
     document.getElementById("accordian-down").style.height="270px"
     document.getElementById("plus").style.display="none"
-
-}
+    document.getElementById("cross").style.display="block"
+    }
 function closeNav(){
-    document.getElementById("cross").style.display="inline"
-}
+        document.getElementById("accordian-down").style.height="0px"
+        document.getElementById("plus").style.display="block"
+        document.getElementById("cross").style.display="none"
+    }
+
+
+function openNav1(){
+    document.getElementById("accordian-down1").style.height="270px"
+    document.getElementById("plus1").style.display="none"
+    document.getElementById("cross1").style.display="block"
+    }
+function closeNav1(){
+        document.getElementById("accordian-down1").style.height="0px"
+        document.getElementById("plus1").style.display="block"
+        document.getElementById("cross1").style.display="none"
+    }
